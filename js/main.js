780786ce56befb47b7b99bf8ea6ee950d9207e5e
franco-giordano <francogior98@gmail.com>
2021-01-28 12:49:38 -0300

    portear y parsear mapa, cambiar colores

--- js/main.js
+++ js/main.js
@@ -1,54 +1,104 @@
-// create an array with nodes
-var nodes = new vis.DataSet([{
-        id: 1,
-        label: 'Mas Screentime'
-    },
-    {
-        id: 2,
-        label: 'Node 2'
-    },
-    {
-        id: 3,
-        label: 'Node 3'
-    },
-    {
-        id: 4,
-        label: 'Node 4'
-    },
-    {
-        id: 5,
-        label: 'Node 5'
+$.ajax({
+    type: "GET",
+    url: "data/mapa.csv",
+    dataType: "text",
+    success: dibujarMapa
+});
+
+// function getRandomInt(max) {
+//     return Math.floor(Math.random() * Math.floor(max));
+//   }
+
+// const SEED = getRandomInt(10000);
+// console.log("SEED: ", SEED);
+
+
+function dibujarMapa(data) {
+    var mapa = $.csv.toObjects(data);
+    console.log(mapa);
+
+    var nodosAdaptados = convertirNodos(mapa);
+    var nodos = new vis.DataSet(nodosAdaptados);
+
+    var aristasAdaptadas = convertirAristas(mapa);
+    var aristas = new vis.DataSet(aristasAdaptadas);
+
+    var container = document.getElementById('mynetwork');
+
+    var data = {
+        nodes: nodos,
+        edges: aristas
+    };
+    var options = {
+        nodes: {
+            shape: 'box',
+            color: '#a3ddcb',
+            margin: 20,
+            shadow: {
+                enabled: true
+            },
+            widthConstraint: {
+                maximum: 150
+            }
+        },
+        edges: {
+            arrows: 'to',
+            width: 6
+        },
+        physics: {
+            barnesHut: {
+                gravitationalConstant: -40000
+            },
+        },
+        layout: {
+            randomSeed: 7403
+        }
+        // layout: {hierarchical: {enabled: true, direction: 'LR', levelSeparation: 400}},
+    };
+
+    var network = new vis.Network(container, data, options);
+
+}
+
+function convertirNodos(mapa) {
+    var nodos = [];
+
+    for (const fila of mapa) {
+        var nuevo = {};
+        nuevo['id'] = fila['id'];
+        nuevo['label'] = fila['label'];
+
+        if (fila['opciones']) {
+            var opciones = fila['opciones'].replaceAll("'", "\"");
+            opciones = JSON.parse(opciones);
+            nuevo = {
+                ...nuevo,
+                ...opciones
+            };
+        }
+
+        nodos.push(nuevo);
     }
-]);
 
-// create an array with edges
-var edges = new vis.DataSet([{
-        from: 1,
-        to: 3
-    },
-    {
-        from: 1,
-        to: 2
-    },
-    {
-        from: 2,
-        to: 4
-    },
-    {
-        from: 2,
-        to: 5
+    return nodos;
+
+}
+
+function convertirAristas(mapa) {
+    var aristas = [];
+
+    for (const fila of mapa) {
+        var conexiones = fila['conexiones'].split('-');
+
+        if (conexiones[0] === "") continue;
+
+        for (const conexion of conexiones) {
+            var nueva = {}
+            nueva['from'] = fila['id'];
+            nueva['to'] = conexion;
+            aristas.push(nueva);
+        }
     }
-]);
 
-// create a network
-var container = document.getElementById('mynetwork');
-
-// provide the data in the vis format
-var data = {
-    nodes: nodes,
-    edges: edges
-};
-var options = {};
-
-// initialize your network!
-var network = new vis.Network(container, data, options);
+    return aristas;
+}
