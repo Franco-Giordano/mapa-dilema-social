494b0a226cb0a388a0bcf9662b1f3437f49ea036
franco-giordano <francogior98@gmail.com>
2021-01-28 15:58:47 -0300

    mejorar estilos e incorporar ionic

--- js/main.js
+++ js/main.js
@@ -29,35 +29,10 @@ function dibujarMapa(data) {
         nodes: nodos,
         edges: aristas
     };
-    var options = {
-        nodes: {
-            shape: 'box',
-            color: '#a3ddcb',
-            margin: 20,
-            font: {size: 20},
-            shadow: {
-                enabled: true
-            },
-            widthConstraint: {
-                maximum: 150
-            }
-        },
-        edges: {
-            arrows: 'to',
-            width: 6
-        },
-        physics: {
-            barnesHut: {
-                gravitationalConstant: -40000
-            },
-        },
-        layout: {
-            randomSeed: 7403
-        }
-        // layout: {hierarchical: {enabled: true, direction: 'LR', levelSeparation: 400}},
-    };
 
-    var network = new vis.Network(container, data, options);
+    var network = new vis.Network(container, data, NETWORK_OPTIONS);
+    network.once("beforeDrawing", () => network.fit());
+    network.once("afterDrawing", () => network.focus(25, {animation: {duration: 2500}, scale: 1}));
 
 }
 
@@ -69,12 +44,16 @@ function convertirNodos(mapa) {
         nuevo['id'] = fila['id'];
         nuevo['label'] = fila['label'];
 
-        if (fila['opciones']) {
-            var opciones = fila['opciones'].replaceAll("'", "\"");
-            opciones = JSON.parse(opciones);
+        if (fila['categoria'] && ESTILOS[fila['categoria']]) {
             nuevo = {
                 ...nuevo,
-                ...opciones
+                ...ESTILOS[fila['categoria']]
+            };
+        }
+        else {
+            nuevo = {
+                ...nuevo,
+                ...ESTILOS.NORMAL
             };
         }
 
---
+++ js/network-options.js
@@ -0,0 +1,66 @@
+const ESTILOS = {
+    CENTRO: {
+        color: '#ffc409',
+        margin: 20,
+        font: {
+            size: 50
+        },
+        widthConstraint: {
+            maximum: 250
+        }
+    },
+    NORMAL: {
+        color: '#a3ddcb',
+        margin: 20,
+        font: {
+            size: 20
+        },
+        widthConstraint: {
+            maximum: 150
+        }
+    },
+    CICLO: {
+        color: '#42d77d',
+        margin: 20,
+        font: {
+            size: 20
+        },
+        widthConstraint: {
+            maximum: 150
+        }
+    },
+    CONSECUENCIA: {
+        color: '#ed576b',
+        margin: 20,
+        font: {
+            size: 20
+        },
+        widthConstraint: {
+            maximum: 200
+        }
+    },
+}
+
+const NETWORK_OPTIONS = {
+    nodes: {
+        shape: 'box',
+        shadow: {
+            enabled: true
+        }
+    },
+    edges: {
+        arrows: 'to',
+        width: 6,
+        color: {inherit: 'to'}
+    },
+    physics: {
+        barnesHut: {
+            gravitationalConstant: -40000
+        },
+    },
+    layout: {
+        randomSeed: 1125
+    },
+    // manipulation: {enabled: true}
+    // layout: {hierarchical: {enabled: true, direction: 'LR', levelSeparation: 400}},
+};
